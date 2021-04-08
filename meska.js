add9d2d2e6ea874725d98d4c003735ea16b05969
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-04-08 12:25:21 +0300

    Meska

---
+++ meska.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const Discord = require("discord.js");
+const fs = require("fs");
+const Meska = require("meska.js");
+const Logger = new Meska.Logger();
+
+fs.readFile(
+  "tokens.txt",
+  { encoding: "utf8", flag: "r" },
+  async function (err, data) {
+    if (err) {
+      Logger.error("FILE", err + "!", true);
+    } else if(data == "") {
+      Logger.error("FILE", "tokens.txt file is empty!", true);
+    } else {
+      let connected = 0;
+      let logined = 0;
+      let tokens = data.split("\n");
+      tokens.forEach(async (token) => {
+        const client = new Discord.Client();
+        await client
+          .login(token)
+          .then(() => {
+            Logger.info("LOGIN", client.user.tag + " is Connected! [Activity: " + process.env.ACTIVITY + " | Status: " + process.env.STATUS + "]", true);
+            setTimeout(() => {
+              client.user.setPresence({
+                activity: {
+                  name: process.env.ACTIVITY,
+                },
+                status: process.env.STATUS,
+              });
+            }, 10000);
+            connected += 1;
+          })
+          .catch((err) => {
+            Logger.error("LOGIN", err + "!", true);
+          });
+        logined += 1;
+        if (logined == tokens.length) {
+          Logger.success("LOGIN", connected + " out of " + logined + " tokens work.", true);
+        }
+      });
+    }
+  }
+);
---
+++ start.bat
@@ -0,0 +1,7 @@
+@echo Meska Token Activator
+@echo off
+title Meska Token Activator
+:x
+node meska.js
+goto x;
+
