bd27c7fd81e7b1c1f20f6c32c785d30989608b31
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-04-26 23:08:48 +0300

    Update meska.js

--- meska.js
+++ meska.js
@@ -34,3 +34,8 @@ fs.readFile(
     }
   }
 );
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.log('Unhandled Rejection at:', promise, 'reason:', reason);
+  // Application specific logging, throwing an error, or other logic here
+});
