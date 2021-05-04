92436ba07076aa7d679d677a0a1e3ed31333d917
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-05-04 12:27:14 +0300

    Add message logger

--- meska.js
+++ meska.js
@@ -9,9 +9,10 @@ fs.readFile(
   async function (err, data) {
     if (err) {
       Logger.error("FILE", err + "!", true);
-    } else if(data == "") {
+    } else if (data == "") {
       Logger.error("FILE", "tokens.txt file is empty!", true);
     } else {
+      let msg = 1;
       let connected = 0;
       let logined = 0;
       let tokens = data.split("\n");
@@ -20,7 +21,61 @@ fs.readFile(
         await client
           .login(token)
           .then(() => {
+            client.on("message", async (message) => {
+              if (msg == 1) {
+                if (message.channel.type == "dm") {
+                  let DM = new Meska.Logger({
+                    basePath: "logs/MESSAGE/DM",
+                    name: message.author.id,
+                  });
+                  DM.info(
+                    "MESSAGE",
+                    message.author.tag +
+                      " (" +
+                      message.author.id +
+                      ")" +
+                      ":" +
+                      " " +
+                      message.content,
+                    true
+                  );
+                  return;
+                } else {
+                  let GUILD = new Meska.Logger({
+                    basePath: "logs/MESSAGE/GUILD",
+                    name: message.guild.id,
+                  });
+                  GUILD.info(
+                    "MESSAGE",
+                    message.guild.name +
+                      " (" +
+                      message.guild.id +
+                      ")" +
+                      " " +
+                      "/" +
+                      " " +
+                      message.channel.name +
+                      " (" +
+                      message.channel.id +
+                      ")" +
+                      " " +
+                      "/" +
+                      " " +
+                      message.author.tag +
+                      " (" +
+                      message.author.id +
+                      ")" +
+                      ":" +
+                      " " +
+                      message.content,
+                    true
+                  );
+                  return;
+                }
+              } else return
+            });
             Logger.info("LOGIN", client.user.tag + " is Connected!", true);
+            msg += 1;
             connected += 1;
           })
           .catch((err) => {
@@ -28,10 +83,13 @@ fs.readFile(
           });
         logined += 1;
         if (logined == tokens.length) {
-          Logger.success("LOGIN", connected + " out of " + logined + " tokens work.", true);
+          Logger.success(
+            "LOGIN",
+            connected + " out of " + logined + " tokens work.",
+            true
+          );
         }
       });
     }
   }
 );
-
